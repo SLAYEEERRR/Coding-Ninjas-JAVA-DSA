6ebe63f6926a948eaa5dda5de849b3d737e13e98
Abhishek Kumar Gupta <89450383+SLAYEEERRR@users.noreply.github.com>
2022-11-27 23:15:50 +0530

    Add files via upload

---
+++ Lecture 3/FloatingNumbers.java
@@ -0,0 +1,10 @@
+/*Given two floating numbers, ‘F1’ and ‘F2’, write a program to return the product of these floating numbers.
+Give your answer to 6 decimal places.*/
+
+public class FloatingNumbers {
+    public static double FloatingNumbers(double f1, double f2) {
+        // Write your code here
+        double product=f1*f2;
+        return product;
+    }
+}
